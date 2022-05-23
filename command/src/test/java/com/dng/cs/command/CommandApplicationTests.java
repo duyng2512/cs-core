327f74fdda00e878a395bb15c0cb2fb8944d509c
duyng2512 <duyng2512@gmail.com>
2022-05-23 14:37:41 +0700

    push source

---
+++ command/src/test/java/com/dng/cs/command/CommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dng.cs.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
